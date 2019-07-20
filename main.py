7ad864c56c898bbbb481b365e57d4f15c4f692ae
rich-sykes <Scorpion1987!>
2019-07-20 20:38:52 +0100

    calculation engine initial commit

--- main.py
+++ main.py
@@ -19,83 +19,163 @@ Reporting Parameters:
 - Month-to-date P&L
 - Year-to-date P&L
 
-Information should be presented in 3 levels of aggregation: Instrument asset class, instrument and contract.
+Information should be presented in 3 levels of aggregation:
+- Instrument asset class
+- instrument
+- contract.
 
 The aggregation level can be an option or all three can be provided in separate tables.
 
 """
 
-# init
-reporting_date = "02/05/2019"
-
-
-# positions held
-from libs.data import get_trade_data, get_contract_data, get_price_data
 
 import pandas as pd
+import datetime
 
+from libs.data import get_instrument_data, get_contract_data, get_trade_data, get_price_data
+# positions held
+
+
+
+##  init
+reporting_date = "03/05/2019"
+
+
+# date functions
+# report date
+current_date = pd.to_datetime(reporting_date, format="%d/%m/%Y")
+
+# aggregation filter  - asset class
+
+instrument_data = get_instrument_data()
+contract_data = get_contract_data()
 trade_data = get_trade_data()
+price_data = get_price_data()
 
-# function dev
-def filter_date(trade_data, reporting_date):
 
-    date_dt = pd.to_datetime(reporting_date, format="%d/%m/%Y")
+# instrument_asset_classes = instrument_data['Instrument Asset Class'].unique().tolist()
+# i.e. 'Equity' / 'Commidities'
+instrument_asset_class = 'Commodities'
+instrument_codes = instrument_data[instrument_data['Instrument Asset Class'] == instrument_asset_class]['Instrument Code'].tolist()
+contract_data = get_contract_data()
+contract_tickers = contract_data[contract_data['Instrument Code'].isin(instrument_codes)]['Contract Ticker'].to_list()
 
-    trade_data = trade_data[trade_data.index <= date_dt]
 
-    return trade_data
+# aggregation filter  - instruments
+instrument_description = 'Russel 2000'
 
+instrument_codes = instrument_data[instrument_data['Instrument Description'] == instrument_description]['Instrument Code'].tolist()
+contract_tickers = contract_data[contract_data['Instrument Code'].isin(instrument_codes)]['Contract Ticker'].to_list()
+
+
+
+# aggregation filter  - contract or contracts
 contract_tickers = ["CCN9 Comdty", "CDM9 Curncy"]
 
 
-def filter_contract(trade_data, contract_names):
 
+
+
+# aggregation_level = 'instrument_asset_class'
+# aggregation_level = 'instrument'
+# aggregation_level = 'contract'
+
+def agg_contracts(trade_data, contract_data, price_data, instrument_data, contract_tickers, current_date, aggregation_level):
+
+    # financial dates
+    # month start date
+    month_to_date = current_date.replace(day=1)
+
+    # month start date
+    year_to_date = current_date.replace(day=1, month=1)
+
+
+    trade_data = trade_data[trade_data.index <= current_date]
     trade_data = trade_data[trade_data['Contract Ticker'].isin(contract_tickers)]
 
-    return trade_data
-
-
-"""
-def positions_held(trade_data, reporting_date):
-"""
-
-
-def agg_contracts(trade_data, contract_tickers, date_dt):
-
-    contract_data = get_contract_data()
-
-
-    df = pd.merge(trade_data.reset_index(), contract_data[['Contract Ticker', 'Contract Multiplier']],
+    # enrich with contract info
+    df = pd.merge(trade_data.reset_index(),
+                  contract_data[['Contract Ticker', 'Contract Description', 'Instrument Code', 'Contract Multiplier']],
                   on=['Contract Ticker'])
 
-    # daily price
-    price_data = get_price_data()
+    # enrich with instrument info
+    df = pd.merge(df,
+                  instrument_data[[
+                      'Instrument Code', 'Instrument Description', 'Instrument Asset Class', 'Instrument Currency']],
+                  on=['Instrument Code'])
 
     # filter to by contracts
     price_data = price_data[contract_tickers]
-    price_data = price_data[contract_tickers][price_data.index <= date_dt]
+    price_data = price_data[contract_tickers][price_data.index <= current_date]
     df_p = pd.melt(price_data.reset_index(),
                    id_vars='Date',
                    value_vars=contract_tickers,
                    var_name='Contract Ticker',
                    value_name='Daily Price'
-                   ).set_index('Date')
+                   )
+
     # final price
-    price_data_final = price_data[contract_tickers][price_data.index == date_dt]
+    price_data_final = price_data[contract_tickers][price_data.index == current_date]
     df_pf = pd.melt(price_data_final.reset_index(),
                    id_vars='Date',
                    value_vars=contract_tickers,
                    var_name='Contract Ticker',
                    value_name='Final Price'
-                   ).set_index('Date')
+                   )
 
-    df_merge = pd.merge(df, df_p) #.set_index('Trade Date')
+    df_merge = pd.merge(df, df_p,
+                        left_on=['Trade Date', 'Contract Ticker'],
+                        right_on=['Date', 'Contract Ticker'])
 
-    df_merge = pd.merge(df_merge, df_pf).set_index('Trade Date')
+    df_merge = pd.merge(df_merge, df_pf[['Final Price', 'Contract Ticker']],
+                        left_on=['Contract Ticker'],
+                        right_on=['Contract Ticker'])
+
+
+    # sort by date
+    df_merge.set_index('Date', inplace=True)
+    df_merge.sort_index(inplace=True)
 
     # daily p/l
     # PriceChange * Multiplier * TradeAmount * IF(SELL, -1, 1)
-    df_merge
+    df_merge['Daily Price Change'] = df_merge['Daily Price'] - df_merge['Avg Price Traded']
+    df_merge['Daily P/L'] = df_merge['Daily Price Change'] * \
+                            df_merge['Contract Multiplier'] * \
+                            df_merge['Traded Amount']
+
+    # overall p/l
+    df_merge['Price Change'] = df_merge['Final Price'] - df_merge['Avg Price Traded']
+    df_merge['P/L'] = df_merge['Price Change'] * \
+                            df_merge['Contract Multiplier'] * \
+                            df_merge['Traded Amount']
+
+
+    df_merge = df_merge[['Trade Date', 'Contract Ticker', 'Contract Description',
+                         'Instrument Code', 'Instrument Description', 'Instrument Asset Class', 'Instrument Currency',
+                         'Contract Multiplier', 'Traded Amount', 'Avg Price Traded',
+                         'Daily Price', 'Daily Price Change', 'Daily P/L',
+                         'Final Price', 'Price Change', 'P/L']]
+
+    # aggregation_level = 'Instrument Asset Class' 'instrument_asset_class'
+    # aggregation_level = 'Instrument Description' 'instrument'
+    # aggregation_level = 'Contract Ticker' 'contract'
+
+    df_overall = df_merge.groupby(by=aggregation_level)[aggregation_level, 'P/L'].sum()
+
+    # Daily P&L
+    df_daily = df_merge[df_merge.index >= current_date].groupby(by=aggregation_level)['Contract Ticker', 'P/L'].sum()
+
+    # Month-to-date P&L
+    df_month = df_merge[df_merge.index >= month_to_date].groupby(by=aggregation_level)['Contract Ticker', 'P/L'].sum()
+
+    # Year-to-date P&L
+    df_year = df_merge[df_merge.index >= year_to_date].groupby(by=aggregation_level)['Contract Ticker', 'P/L'].sum()
+
+    # Position held in the portfolio( in contracts)
+    # sum units
+
+
+    # Valuation sum units * current price
 
 
 

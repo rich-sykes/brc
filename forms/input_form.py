91e9d50ade52a93cbdadf2ec7c9807602beb8c19
rich-sykes <Scorpion1987!>
2019-07-21 23:04:38 +0100

    completed aggregation function

--- forms/input_form.py
+++ forms/input_form.py
@@ -1,10 +1,14 @@
-from wtforms import Form, SelectField, DateField
+from wtforms import Form, SelectField, StringField, DateField, validators
+from flask_wtf import FlaskForm
 
 
-class PortfolioAgg(Form):
+class PortfolioAgg(FlaskForm):
 
-    reporting_date = DateField('Report Date', format='%d/%m/%Y')
+    reporting_date = StringField('Report Date') #, format='%d/%m/%Y'
 
     aggregation_level = SelectField('Aggregation Level',
-                                    choices=[('Instrument Asset Class' , 'Instrument Description', 'Contract Ticker'),
-                                             ('Instrument Asset Class' , 'Instrument Description', 'Contract Ticker')])
+                                    choices=[('Instrument Asset Class', 'Instrument Asset Class'),
+                                             ('Instrument Description', 'Instrument Description'),
+                                             ('Contract Ticker', 'Contract Ticker')]
+                                    )
+
